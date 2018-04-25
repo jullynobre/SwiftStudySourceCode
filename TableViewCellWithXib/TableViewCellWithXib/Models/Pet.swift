b3cfd7b6d6db23c84081ce3f64807979a6401dcb
jullynobre <jullynobre1@gmail.com>
2018-04-25 10:07:47 -0300

    Add images

---
+++ TableViewCellWithXib/TableViewCellWithXib/Models/Pet.swift
@@ -0,0 +1,9 @@
+//
+//  Pet.swift
+//  TableViewCellWithXib
+//
+//  Created by Ada 2018 on 25/04/18.
+//  Copyright © 2018 Academy 2018. All rights reserved.
+//
+
+import Foundation
---
+++ TableViewCellWithXib/TableViewCellWithXib/PetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PetTableViewCell.swift
+//  TableViewCellWithXib
+//
+//  Created by Ada 2018 on 25/04/18.
+//  Copyright © 2018 Academy 2018. All rights reserved.
+//
+
+import UIKit
+
+class PetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

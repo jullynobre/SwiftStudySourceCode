30291ca52845aceed3d695084767fb3df56faf93
jullynobre <jullynobre1@gmail.com>
2018-05-07 09:40:32 -0300

    Question CollectioView

---
+++ CollectionView/ChallengeColectionView/ChallengeColectionView/Models/CollectionItem.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionItem.swift
+//  ChallengeColectionView
+//
+//  Created by Ada 2018 on 07/05/18.
+//  Copyright © 2018 Academy 2018. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CollectionItem {
+    
+    var backgroundColor: UIColor
+    var number: Int
+    
+    
+}
---
+++ CollectionView/ChallengeColectionView/ChallengeColectionView/Views/ItemCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ItemCollectionViewCell.swift
+//  ChallengeColectionView
+//
+//  Created by Ada 2018 on 07/05/18.
+//  Copyright © 2018 Academy 2018. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

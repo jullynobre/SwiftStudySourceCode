690a5949eee36f75ba3784dc151cbeb36f6ff91a
jullynobre <jullynobre1@gmail.com>
2018-10-08 11:31:00 -0300

    Converting devicetoken from data to string

--- PushNotification/PushNotification/AppDelegate.swift
+++ PushNotification/PushNotification/AppDelegate.swift
@@ -31,7 +31,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        print(deviceToken)
+        
+        let tokenParts = deviceToken.map { (data) -> String in
+            return String(format: "%02.2hhx", data)
+        }
+        let token = tokenParts.joined()
+        
+        print("Device Token: \(token)")
     }
 }
 

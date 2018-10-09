7219f5ba6cc5b72bf4a455fd954824c2e8982002
jullynobre <jullynobre1@gmail.com>
2018-10-09 16:46:46 -0300

    Add ContentExtension to Notifications

---
+++ Notifications/ContentExtension/ContentExtension/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  ContentExtension
+//
+//  Created by Jully Nobre on 09/10/18.
+//  Copyright © 2018 Jully Nobre. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+            if error != nil { print(error!) }
+            else if success {
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
+            }
+        }
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map { (data) -> String in
+            return String(format: "%02.2hhx", data)
+        }
+        let token = tokenParts.joined()
+        
+        print("Device Token: \(token)")
+    }
+
+    
+}
+

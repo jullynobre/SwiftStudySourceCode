6ee3843e6e8d55cb9d22ec4af48be1c6fb71880e
jullynobre <jullynobre1@gmail.com>
2018-10-09 08:59:21 -0300

    Add method setNotificationCategory to RichNotification

--- RichNotification/RichNotification/ViewController.swift
+++ RichNotification/RichNotification/ViewController.swift
@@ -1,20 +1,54 @@
 //
 //  ViewController.swift
-//  RichNotification
+//  LocalNotification
 //
-//  Created by Jully Nobre on 09/10/18.
+//  Created by Jully Nobre on 08/10/18.
 //  Copyright © 2018 Jully Nobre. All rights reserved.
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-
+    let categotyId = "categoryID"
+    let actionID = "actionID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.setNotificationCategories()
+        UNUserNotificationCenter.current().delegate = self
+        
+        createNotification(title: "Teste", subtitle: "testando", body: "Foi o Guimbo quem fez esse video. <3")
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    func setNotificationCategories(){
+        let okAction = UNNotificationAction(identifier: self.actionID,
+                                            title: "OK",
+                                            options: [])
+        let category = UNNotificationCategory(identifier: self.categotyId,
+                                              actions: [okAction],
+                                              intentIdentifiers: [],
+                                              options: [.customDismissAction])
+        UNUserNotificationCenter.current().setNotificationCategories([category])
+    }
+    
+    func createNotification(title: String, subtitle: String, body: String) {
+        let notification = UNMutableNotificationContent()
+        notification.title = title
+        notification.subtitle = subtitle
+        notification.body = body
+        notification.categoryIdentifier = categotyId
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        UNUserNotificationCenter.current().add(UNNotificationRequest.init(identifier: "Local Notification", content: notification, trigger: trigger), withCompletionHandler: nil)
+    }
+}
+extension ViewController: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    }
 }
 
